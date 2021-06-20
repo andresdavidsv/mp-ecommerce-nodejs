b092ba03304952f0a1db872d8c48ad792a33030b
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-06-20 13:32:34 -0500

    :sparkles: Relesase

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const port = process.env.PORT || 3000
-const mercadopago = require('mercadopago')
-const bodyparser = require('body-parser');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
 
-let app = express();
+const mercadoPago = require('./controller/mercadopago');
 
-mercadopago.configure({
-  access_token: 'APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803',
-  integrator_id : 'dev_24c65fb163bf11ea96500242ac130004'
-})
+var app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.static('assets'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('assets'));
 
 app.use('/assets', express.static(__dirname + '/assets'));
 
@@ -30,96 +27,24 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
-app.post('/checkout', function (req, res) {
-    const items = [
-        {
-            id: 1234,
-            title: req.body.title,
-            description: "Dispositivo móvil de Tienda e-commerce",
-            picture_url: req.body.img,
-            quantity: parseInt(req.body.unit),
-            unit_price: parseFloat(req.body.price),
-            category_id: 'phones',
-            currency_id: 'COP',
-            external_reference: "andresdavidsolartevidal@gmail.com",
-        }
-    ];
-    const preferences = {
-        items,
-        payer: {
-            name: "Lalo",
-            surname: "Landa",
-            email: "test_user_83958037@testuser.com",
-            identification: {
-                type:'CC',
-                number: "12345678999"
-            },
-            phone: {
-                area_code: "52",
-                number: parseInt(5549737300)
-            },
-            address: {
-                street_name: "Insurgentes Sur",
-                street_number: 1602,
-                zip_code: "03940"
-            }
-        },
-        payment_methods: {
-            excluded_payment_methods: [
-                {
-                    id: "amex"
-                }
-            ],
-            excluded_payment_types: [{ id: "atm" }],
-            installments: 6,
-        },
-        back_urls: {
-            success: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/success",
-            failure: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/failure",
-            pending: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/pending"
-        },
-        notification_url: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/webhook",
-        auto_return: "approved"
-    };
-    mercadopago.preferences.create(preferences)
-        .then(function (response) {
-            // Este valor reemplazará el string "<%= global.id %>" en tu HTML
-            global.id = response.body.id;
+app.post('/checkout', mercadoPago.creteReference);
 
-            console.log("Preferences id -> "+response.body.id);
-            console.log(response);
-
-            res.redirect(response.body.init_point);
-
-        }).catch(function (error) {
-        console.log(error);
-    });
+app.get('/success', (req, res) => {
+    res.render('success', req.query)
 });
 
-app.get('/success', function (req, res) {
-    res.render('success', req.query);
-});
-app.get('/failure', function (req, res) {
+app.get('/failure', (req, res) => {
     res.render('failure', req.query);
 });
-app.get('/pending', function (req, res) {
+
+app.get('/pending', (req, res) => {
     res.render('pending', req.query);
 });
 
-app.post('/webhook', function (req, res) {
-    console.log(req);
-    console.log(res);
-    let body = ""; 
-    req.on("data", chunk => {  
-    body += chunk.toString();
-    });
-    req.on("end", () => {  
-    console.log(body, "webhook response"); 
-    res.end("ok",body);
-    });
-    return res.status(200);
-});
-
+app.post('/webhook', (req, res) => {
+    console.log(req.body, "Webhook");
+    res.status(200).send('ok');
+})
 
 app.listen(port, function () {
     console.log(`Listening http://localhost:${port}`);
---
+++ controller/mercadopago.js
@@ -0,0 +1,70 @@
+var mercadopago = require('mercadopago');
+mercadopago.configure({
+    access_token: 'APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
+});
+
+const mercadoCheckout = {};
+
+mercadoCheckout.creteReference = async (req, res) => {
+    const { body } = req;
+
+    const preference = {
+        items: [
+            {
+                id: 1234,
+                title: body.title,
+                description: 'DispositivomóvildeTiendae-commerce',
+                picture_url: 'https://aldairl-mp-ecommerce-nodejs.herokuapp.com/assets/003.jpg',
+                quantity: Number(body.unit),
+                currency_id: 'COP',
+                unit_price: Number(body.price)
+            }
+        ],
+        payer: {
+            name: "Lalo",
+            last_name: "Landa",
+            email: "test_user_83958037@testuser.com",
+            date_created: "2015-06-02T12:58:41.425-04:00",
+            phone: {
+                area_code: "52",
+                number: 5549737300
+            },
+            address: {
+                street_name: "Insurgentes Sur",
+                street_number: 1602,
+                zip_code: "03940"
+            }
+        },
+        external_reference: "andresdavidsolartevidal@gmail.com",
+        payment_methods: {
+            excluded_payment_methods: [
+            ],
+            excluded_payment_types: [
+                {
+                    id: "amex"
+                },
+                {
+                    id: "atm"
+                }
+            ],
+            installments: 6
+        },
+        back_urls: {
+            success: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/success",
+            failure: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/failure",
+            pending: "https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/pending"
+        },
+        notification_url: 'https://andresdavi-mp-ecommerce-nodejs.herokuapp.com/webhook',
+        auto_return: "approved"
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(response => {
+            console.log(response.body);
+            res.redirect(response.body.init_point);
+        })
+        .catch(e => console.log(e))
+}
+
+module.exports = mercadoCheckout;
